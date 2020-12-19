0da25608e2bd9907692186a0f0c407ea19e36512
Olga Lee <olyalee100@gmail.com>
2020-12-19 23:13:55 +0300

    доверстала каталог

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,21 +1,26 @@
-
 let modalOrder = document.querySelector(".modal-order__wrapper");
-let openModalOrder = document.querySelector(".featured__link");
+let product = document.querySelectorAll(".catalog__item");
+let openModalOrder = document.querySelectorAll(".order-button");
 let modalClose = document.querySelector(".modal-order__add");
 let sizeS = document.querySelector(".modal-order__label--S");
 
 
-openModalOrder.addEventListener("click", function (evt) {
-evt.preventDefault();
-modalOrder.classList.add("modal-order__open");
+let addClickHandlerBasket = function (openModalOrder) {
+  openModalOrder.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalOrder.classList.add("modal-order__open");
   sizeS.focus();
-});
+  });
+  };
+
+for (var i = 0; i < openModalOrder.length; i++) {
+  addClickHandlerBasket(openModalOrder[i]);
+}
 
 modalClose.addEventListener("click", function (evt) {
 evt.preventDefault();
 modalOrder.classList.remove("modal-order__open");
 });
-
 
 document.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
